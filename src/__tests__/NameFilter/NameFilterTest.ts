7cbb7c4332107b1d81bab200b4a0e3b71031c310
milletf1 <timmiller83@gmail.com>
2019-06-26 19:32:03 +1200

    Setup Jest and created NameFilter test file

---
+++ src/__tests__/NameFilter/NameFilterTest.ts
@@ -0,0 +1,34 @@
+import { IBaseItem } from '../../models/items/IBaseItem';
+import NameFilter from '../../services/filter/filter-modules/name-filter/NameFilter';
+
+test('should return item when searched by full name', () => {
+  throw new Error('not implemented');
+});
+
+test('should return item when searched by partial name', () => {
+  throw new Error('not implemented');
+});
+
+test('should ignore case when filtering items', () => {
+  throw new Error('not implemented');
+});
+
+test('should return item when searched by the name', () => {
+  throw new Error('not implemented');
+});
+
+test('should return item when searched by typeline', () => {
+  throw new Error('not implemented');
+});
+
+test('should return currency', () => {
+  throw new Error('not implemented');
+});
+
+test('should return cards', () => {
+  throw new Error('not implemented');
+});
+
+test('should return maps', () => {
+  throw new Error('not implemented');
+});
--- src/services/filter/filter-modules/name-filter/NameFilter.ts
+++ src/services/filter/filter-modules/name-filter/NameFilter.ts
@@ -1,11 +1,15 @@
+import { array } from 'prop-types';
 import { IBaseItem } from '../../../../models/items/IBaseItem';
-import { IFilterInstructions } from '../IFilterInstructions';
 import { IFilterModule } from './../IFilterModule';
 
 class NameFilter implements IFilterModule<string | string[]> {
   public type: string = 'NameFilter';
 
   public filter(items: IBaseItem[], conditions: string | string[]): IBaseItem[] {
-    return items;
+    const filteredItems: IBaseItem[] = [];
+
+    return filteredItems;
   }
 }
+
+export default NameFilter;
