df1166c193fbffe7fe8bdce6518ad8b8a7ac5f57
milletf1 <timmiller83@gmail.com>
2019-06-27 18:47:43 +1200

    Added test data for name filter tests

--- src/__tests__/NameFilter/NameFilterTest.ts
+++ src/__tests__/NameFilter/NameFilterTest.ts
@@ -1,5 +1,6 @@
 import { IBaseItem } from '../../models/items/IBaseItem';
 import NameFilter from '../../services/filter/filter-modules/name-filter/NameFilter';
+import json from './name-filter-test-items.json';
 
 test('should return item when searched by full name', () => {
   throw new Error('not implemented');
@@ -30,5 +31,13 @@ test('should return cards', () => {
 });
 
 test('should return maps', () => {
+  throw new Error('not implemented');
+});
+
+test('should return a gem that is socketed into an item', () => {
+  throw new Error('not implemented');
+});
+
+test('should return 2 items', () => {
   throw new Error('not implemented');
 });
