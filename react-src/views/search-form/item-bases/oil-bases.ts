ca0a2ab7c297f9000156a9c50e1ce8c0afab5151
milletf1 <timmiller83@gmail.com>
2020-01-29 20:12:59 +1300

    Added oil bases

---
+++ react-src/views/search-form/item-bases/oil-bases.ts
@@ -0,0 +1,53 @@
+import { IItemBase } from './../../../services/filter/filter-modules/item-type-filter/IItemBase';
+import { ItemType } from './../../../services/filter/filter-modules/item-type-filter/ItemType';
+
+export const oilBases: IItemBase[] = [
+  {
+    base: 'Amber Oil',
+    type: ItemType.OIL,
+  },
+  {
+    base: 'Azure Oil',
+    type: ItemType.OIL,
+  },
+  {
+    base: 'Black Oil',
+    type: ItemType.OIL,
+  },
+  {
+    base: 'Clear Oil',
+    type: ItemType.OIL,
+  },
+  {
+    base: 'Crimson Oil',
+    type: ItemType.OIL,
+  },
+  {
+    base: 'Golden Oil',
+    type: ItemType.OIL,
+  },
+  {
+    base: 'Opalescent Oil',
+    type: ItemType.OIL,
+  },
+  {
+    base: 'Sepia Oil',
+    type: ItemType.OIL,
+  },
+  {
+    base: 'Silver Oil',
+    type: ItemType.OIL,
+  },
+  {
+    base: 'Teal Oil',
+    type: ItemType.OIL,
+  },
+  {
+    base: 'Verdant Oil',
+    type: ItemType.OIL,
+  },
+  {
+    base: 'Violet Oil',
+    type: ItemType.OIL,
+  },
+];
